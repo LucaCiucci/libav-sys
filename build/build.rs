0ff1383ff2a53644645684dfa85394f7edf16370
Luca Ciucci <luca.ciucci99@gmail.com>
2023-03-08 22:16:01 +0100

    reorganized

--- build/build.rs
+++ build/build.rs
@@ -1,8 +1,16 @@
 use std::{path::{Path, PathBuf}, fmt::{Debug}, error::Error, fs::create_dir_all};
 
+use bindgen::callbacks::ParseCallbacks;
+
 pub mod env_install;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    // special case for docs.rs
+    if std::env::var("DOCS_RS").is_ok() || true {
+        return empty_bindings();
+    }
+    
     let suite = find_suite()?;
 
     add_linker_search_dir(&suite.lib_dir());
@@ -13,8 +21,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     bindgen_for_suite(suite.as_ref())?;
 
-    //panic!("r:");
+    Ok(())
+}
 
+/// Generate an empty bindings module.
+/// 
+/// This is used when the crate is being built on docs.rs, because docs.rs
+/// does not have access to the system ffmpeg installation.
+fn empty_bindings() -> Result<(), Box<dyn Error>> {
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR")?);
+    let bindings_module_path = out_dir.join("bindings.rs");
+    std::fs::write(&bindings_module_path, "")?;
     Ok(())
 }
 
@@ -22,9 +39,7 @@ pub trait FfmpegSuite: Debug {
     fn include_dir(&self) -> PathBuf;
     fn lib_dir(&self) -> PathBuf;
     fn lib_names(&self) -> Vec<String>;
-    fn main_include_for_lib(&self, lib: &str) -> String {
-        format!("{}.h", lib)
-    }
+    fn main_include_for_lib(&self, lib: &str) -> String;
 }
 
 fn find_suite() -> Result<Box<dyn FfmpegSuite>, Box<dyn std::error::Error>> {
@@ -53,12 +68,13 @@ fn bindgen_for_suite(suite: &dyn FfmpegSuite) -> Result<(), Box<dyn Error>> {
 
     let out_dir = PathBuf::from(std::env::var("OUT_DIR")?);
 
-    //let bindings_outdir = out_dir.join("bindings");
     let bindings_outdir = out_dir.clone();
     create_dir_all(&bindings_outdir)?;
 
-    let mut bindings_modules: Vec<String> = Vec::new();
-    
+    let mut builder = bindgen::Builder::default()
+            .clang_arg(&clang_arg)
+            .parse_callbacks(Box::new(CommentProcessor));
+
     for lib in suite.lib_names() {
         let header = suite.include_dir()
             .join(format!("lib{lib}"))
@@ -68,42 +84,50 @@ fn bindgen_for_suite(suite: &dyn FfmpegSuite) -> Result<(), Box<dyn Error>> {
             return Err(format!("Could not find {header:?}").into());
         }
 
-        let builder = bindgen::Builder::default()
-            .clang_arg(&clang_arg)
-            .header(header.to_string_lossy());
+        builder = builder.header(header.to_string_lossy());
 
-        let bindings = builder
-            // see https://doc.rust-lang.org/beta/rustc/lints/listing/warn-by-default.html
-            // see https://github.com/rust-av/libav-rs/blob/master/libav-sys/build.rs
-            .raw_line("#![allow(deprecated)]")
-            .raw_line("#![allow(dead_code)]")
-            .raw_line("#![allow(non_camel_case_types)]")
-            .raw_line("#![allow(non_snake_case)]")
-            .raw_line("#![allow(non_upper_case_globals)]")
-            .raw_line("#![allow(clashing_extern_declarations)]")
-            .generate()
-            .expect("Unable to generate bindings");
+        //let module_name = lib;
+        //let module_path = bindings_outdir.join(format!("{module_name}.rs"));
 
-        let module_name = lib;
-        let module_path = bindings_outdir.join(format!("{module_name}.rs"));
-
-        bindings
-            .write_to_file(module_path)
-            .expect("Couldn't write bindings!");
-
-        bindings_modules.push(module_name);
+        //bindings_modules.push(module_name);
     }
 
-    let module_content: String = bindings_modules.iter()
-        .map(|module_name| format!("pub mod {module_name};"))
-        .collect::<Vec<String>>()
-        .join("\n");
+    let bindings = builder
+        // see https://doc.rust-lang.org/beta/rustc/lints/listing/warn-by-default.html
+        // see https://github.com/rust-av/libav-rs/blob/master/libav-sys/build.rs
+        //.raw_line("#![allow(deprecated)]")
+        //.raw_line("#![allow(dead_code)]")
+        //.raw_line("#![allow(non_camel_case_types)]")
+        //.raw_line("#![allow(non_snake_case)]")
+        //.raw_line("#![allow(non_upper_case_globals)]")
+        //.raw_line("#![allow(clashing_extern_declarations)]")
+        .generate()
+        .expect("Unable to generate bindings");
 
     let bindings_module_path = out_dir.join("bindings.rs");
-    std::fs::write(&bindings_module_path, module_content)?;
+
+    bindings
+        .write_to_file(bindings_module_path)
+        .expect("Couldn't write bindings!");
+
+    //std::fs::write(&bindings_module_path, module_content)?;
     //panic!("module_path: {:?}\n{:?}\n{:?}", out_dir, out_dir.join("bindings.rs"), bindings_module_path);
 
     //panic!("bindings: {:?}", 1);
 
     Ok(())
+}
+
+
+#[derive(Debug)]
+struct CommentProcessor;
+
+impl ParseCallbacks for CommentProcessor {
+    fn process_comment(&self, comment: &str) -> Option<String> {
+        // TODO doesn't work really well...
+        Some(doxygen_rs::transform(comment))
+    }
+    //fn item_name(&self, _original_item_name: &str) -> Option<String> {
+    //    return Some("aaa::".to_string() + _original_item_name);
+    //}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,27 @@
+//! Rust bindings for system libav libraries
+//! 
+//! This crate searches for system installation of ffmpeg, it tries
+//! to generate bindings for all the 
+
+#![allow(deprecated)]
+#![allow(dead_code)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(clashing_extern_declarations)]
 
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-pub fn hello_there() {
-    println!("Hello, there!");
+/// This module contains the generated bindings for the system libav libraries.
+/// 
+/// 
+/// # Warning
+/// Documentation for this crate cannot be generated on [`docs.rs`](//docs.rs) because it requires
+/// a system installation of ffmpeg.
+/// 
+/// Also, the bindings may be different depending on the particular version of ffmpeg installed.
+/// 
+/// To generate the documentation locally, you can use the `cargo rustdoc --open` command.
+pub mod ffi {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
+
